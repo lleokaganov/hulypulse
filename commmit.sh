45cad856047fba1ca79ba9cf968f106fade0bf75
Leonid Kaganov <lleo@lleo.me>
2025-08-06 09:39:53 +0300

    Readme 2

--- commmit.sh
+++ commmit.sh
@@ -12,6 +12,6 @@ clear
 # git checkout -b feature/${NAME}
 git add .
 #git commit -m "First test redis 5 sec, use: http://127.0.0.1:8095/api/"
-git commit -m "Readme 1"
+git commit -m "Readme 2"
 git push origin
 # feature/${NAME}
